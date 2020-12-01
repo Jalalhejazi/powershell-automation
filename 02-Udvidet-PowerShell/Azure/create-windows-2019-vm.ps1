3c07e062a3399b774989d995e500c191f99a4abf
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-12-01 15:22:34 +0000

    Updated create-windows-2019-vm.ps1

--- 02-Udvidet-PowerShell/Azure/create-windows-2019-vm.ps1
+++ 02-Udvidet-PowerShell/Azure/create-windows-2019-vm.ps1
@@ -5,12 +5,14 @@
 
 az group create -l westeurope -n vm
 
+$secret = "The password length must be between 12 and 123"
+
 az vm create `
     --resource-group vm `
     --name win2019 `
     --image win2019datacenter `
     --admin-username sysadmin `
-    --admin-password 'todo change pwd here'
+    --admin-password $secret
 
 az vm open-port --port 3389 --resource-group vm --name win2019
 
